b9a57d895c860eb4d068135c3a9595b9e1b44169
jbdc0326 <160443063+jbdc0326@users.noreply.github.com>
2024-03-25 02:16:00 +0800

    Initial commit

--- scripts/tic-tac-toe-scripts.js
+++ scripts/tic-tac-toe-scripts.js
@@ -20,10 +20,10 @@ function checkWinner() {
     // Check rows
     for (let i = 0; i < 3; i++) {
         if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-            //alert(`${board[i][0]} wins!`);
-            document.getElementById('modal-winner-body').innerHTML=`<img class="current-player-small" src="../images/${board[i][0]}.png" alt="${board[i][0]}"> wins!`;
-            var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
-            modal.toggle();
+            alert(`${board[i][0]} wins!`);
+            // document.getElementById('modal-winner-body').innerHTML=`<img class="current-player-small" src="../images/${board[i][0]}.png" alt="${board[i][0]}"> wins!`;
+            // var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
+            // modal.toggle();
             reset();
             return;
         }
@@ -32,10 +32,10 @@ function checkWinner() {
     // Check columns
     for (let j = 0; j < 3; j++) {
         if (board[0][j] !== '' && board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
-            //alert(`${board[0][j]} wins!`);
-            document.getElementById('modal-winner-body').innerHTML=`<img class="current-player-small" src="../images/${board[0][j]}.png" alt="${board[0][j]}"> wins!`;
-            var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
-            modal.toggle();
+            alert(`${board[0][j]} wins!`);
+            // document.getElementById('modal-winner-body').innerHTML=`<img class="current-player-small" src="../images/${board[0][j]}.png" alt="${board[0][j]}"> wins!`;
+            // var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
+            // modal.toggle();
             reset();
             return;
         }
@@ -43,28 +43,28 @@ function checkWinner() {
 
     // Check diagonals
     if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-        //alert(`${board[0][0]} wins!`);
-        document.getElementById('modal-winner-body').innerHTML=`<img class="current-player-small" src="../images/${board[0][0]}.png" alt="${board[0][0]}"> wins!`;
-        var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
-        modal.toggle();
+        alert(`${board[0][0]} wins!`);
+        // document.getElementById('modal-winner-body').innerHTML=`<img class="current-player-small" src="../images/${board[0][0]}.png" alt="${board[0][0]}"> wins!`;
+        // var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
+        // modal.toggle();
         reset();
         return;
     }
     if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-        //alert(`${board[0][2]} wins!`);
-        document.getElementById('modal-winner-body').innerHTML=`<img class="current-player-small" src="../images/${board[0][2]}.png" alt="${board[0][2]}"> wins!`;
-        var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
-        modal.toggle();
+        alert(`${board[0][2]} wins!`);
+        // document.getElementById('modal-winner-body').innerHTML=`<img class="current-player-small" src="../images/${board[0][2]}.png" alt="${board[0][2]}"> wins!`;
+        // var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
+        // modal.toggle();
         reset();
         return;
     }
 
     // Check for a tie
     if (!board.flat().includes('')) {
-        //alert("It's a tie!");
-        document.getElementById('modal-winner-body').innerHTML="It's a tie!";
-        var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
-        modal.toggle();
+        alert("It's a tie!");
+        // document.getElementById('modal-winner-body').innerHTML="It's a tie!";
+        // var modal = new bootstrap.Modal(document.getElementById('winnerModal'));
+        // modal.toggle();
         reset();
         return;
     }
